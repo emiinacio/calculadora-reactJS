5a0b4f5b018743e000e2e00d7c67e351cd3754d3
Emily Marcelino Inácio <55949281+emiinacio@users.noreply.github.com>
2022-01-21 11:18:12 +0000

    desgin finish

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Calculator from './main/Calculator';
 
+
 ReactDOM.render(
   <div>
       <h1>Caluladora</h1>
--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,15 +1,14 @@
-import React, { Component } from "react";
-import "./Calculator.css";
+import React, { Component } from "react"
+import "./Calculator.css"
 
-
-import Button  from '../components/Button';
-import Display from '../components/Display';
+import Button  from '../components/Button'
+import Display from '../components/Display'
 
 export default class Calculator extends Component {
     render() {
-        return(
+        return (
             <div className='calculator'>
-                <Display value={100}/>
+                <Display value={100} />
                 <Button label='AC'/> 
                 <Button label='/'/> 
                 <Button label='7'/>
